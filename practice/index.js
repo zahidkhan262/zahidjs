061113d1ec4717059e56f6245c4c8b8e7a4ff74c
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2023-04-04 03:07:48 +0530

    find duplicate value

--- practice/index.js
+++ practice/index.js
@@ -74,4 +74,19 @@ for (let x = 0; x < name.length; x++) {
     }
 
 }
-console.log(obj2, "obj2")
+console.log(obj2, "obj2");
+
+
+let data = [20, 20, 40, 2, 30, 50, 2, 30, 55, 50, 1, 6, 9, 9]
+
+const findDuplicateValue = (arr) => {
+    const hash = {}
+
+    for (let i = 0; i < arr.length; i++) {
+        hash[arr[i]] = arr[i]
+        console.log(hash, "zahid")
+    }
+    return Object.values(hash)
+
+}
+console.log(findDuplicateValue(data))
