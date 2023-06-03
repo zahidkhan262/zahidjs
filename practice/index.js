4e057e66d0b93d9361caf3d2ccaebd5ce7e458f9
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2023-06-03 14:37:28 +0530

    Update index.js

--- practice/index.js
+++ practice/index.js
@@ -394,3 +394,15 @@ for (let i = 0; i < data.length; i++) {
 }
 
 console.log(uniqueElements);
+
+//array obj
+const fromPairs = (Array) => {
+    let newObject = {};
+    for (let i = 0; i < Array.length; i++) {
+        newObject[Array[i][0]] = Array[i][1]
+    }
+    return newObject;
+}
+
+const data = [['a', 1], ['b', 2], ['c', 3], ['d', 4]]
+console.log(fromPairs(data))
