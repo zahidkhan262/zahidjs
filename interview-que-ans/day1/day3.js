99bffd1e8260d8962faba7aeef2f72966e2ea134
zahidkhan <zahidkhan007860@mail.com>
2023-06-13 10:11:37 +0530

    find palindrom or not

--- interview-que-ans/day1/day3.js
+++ interview-que-ans/day1/day3.js
@@ -1,15 +1,25 @@
 //find number is prime or not
 
-const checkNumberIsPrime = (num) => {
-    if (num <= 1) {
-        console.log('1 is composite number');
-        return false;
+// const checkNumberIsPrime = (num) => {
+//     if (num <= 1) {
+//         console.log('1 is composite number');
+//         return false;
+//     }
+//     for (let i = 2; i < num; i++) {
+//         if (num % i === 0) {
+//             return false
+//         }
+//     }
+//     return true
+// };
+// console.log(checkNumberIsPrime(21), "__")
+
+
+const findPalindrom = (data) => {
+    let palindrom = '';
+    for (let i = data.length - 1; i >= 0; i--) {
+        palindrom += data[i]
     }
-    for (let i = 2; i < num; i++) {
-        if (num % i === 0) {
-            return false
-        }
-    }
-    return true
-};
-console.log(checkNumberIsPrime(21), "__")
+    return palindrom === data ? true : false
+}
+console.log(findPalindrom('121'))
