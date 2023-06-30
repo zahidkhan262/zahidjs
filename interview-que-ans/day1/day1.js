e20ac9a051e866ac21160ee12c0ad9ed35a8f36a
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2023-06-30 23:08:39 +0530

    Update day1.js

--- interview-que-ans/day1/day1.js
+++ interview-que-ans/day1/day1.js
@@ -14,6 +14,17 @@ const findLargestNumber2 = (arr) => arr.reduce((prev, curr) => prev < curr ? pre
 console.log(findLargestNumber2(nums), "findLargest second way");
 console.log(findLargestNumber(nums), "findLargest first way");
 
+const findLargestNumber =(arr)=>{
+        let largest = 0;
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]>largest){
+        largest = arr[i]
+        }
+    }
+    return largest
+}
+console.log(findLargestNumber(nums))
+
 /// add all ele
 const addElement = (arr) => arr.reduce((prev, curr) => prev += curr, 0)
 
