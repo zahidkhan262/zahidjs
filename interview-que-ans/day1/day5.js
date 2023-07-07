11e2d0c5b849df223b64c21e534972ee4622e3ad
ZAHID KHAN <76157268+zahidkhan262@users.noreply.github.com>
2023-07-07 14:43:42 +0530

    last

--- interview-que-ans/day1/day5.js
+++ interview-que-ans/day1/day5.js
@@ -37,3 +37,43 @@ function subtract(a, b) {
 
 console.log(applyOperation(5, 3, add)); // Output: 8
 console.log(applyOperation(5, 3, subtract)); // Output: 2
+
+
+
+
+
+let x = 0.1 + 0.2;
+// let y = 0.3;
+// console.log(x == y);
+// You2:25 PM
+// false
+// Expert2:25 PM
+// let x = [];
+// console.log(Boolean(x));
+// You2:27 PM
+// true
+// Expert2:27 PM
+// let x = "5";
+// let y = 2;
+  
+// console.log(x + y);
+// console.log(x - y);
+// You2:28 PM
+// x+y=52
+// x-y= NaN
+// Expert2:28 PM
+// console.log(5+"5"-5)
+// You2:29 PM
+// NaN
+// Expert2:29 PM
+// let a = () => {
+//   console.log(this);
+// };
+  
+// a();
+// You2:30 PM
+// undefined
+// Expert2:31 PM
+// let x = [1, 2, 3, 4, 5];
+// Expert2:34 PM
+// output should be 3,4,5
